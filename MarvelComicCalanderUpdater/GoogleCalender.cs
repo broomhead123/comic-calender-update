e486ba65052004c0f6ec2f0ac776cc9fc6f0e111
broomhead123 <callum_collins@hotmail.com>
2015-09-01 18:24:55 +0100

    Inital Commit

---
+++ MarvelComicCalanderUpdater/GoogleCalender.cs
@@ -0,0 +1,59 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace MarvelComicCalanderUpdater {
+	class GoogleCalender {
+
+		string[] Scopes = { CalendarService.Scope.Calendar, CalendarService.Scope.CalendarReadonly };
+		string ApplicationName = "Google Calendar API Quickstart";
+
+
+		public void CreateEvent(string eventName, string date) {
+			//Create new API Service
+			CalendarService service = createCalenderService();
+
+			//Create new event
+			Event newEvent = new Event(){Summary = eventName};
+			newEvent.Start = new EventDateTime() { DateTime = Convert.ToDateTime(date), TimeZone = "Europe/London" };
+			newEvent.End = new EventDateTime() { DateTime = Convert.ToDateTime(date), TimeZone = "Europe/London" };
+			newEvent = service.Events.Insert(newEvent, "callum_collins@hotmail.com").Execute();
+			Console.WriteLine("Event created: {0} \n", newEvent.HtmlLink);
+		}
+
+		private CalendarService createCalenderService() {
+
+			UserCredential credential;
+			using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read)) {
+				string credPath = System.Environment.GetFolderPath(
+					System.Environment.SpecialFolder.Personal);
+				credPath = Path.Combine(credPath, ".credentials");
+
+				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					Scopes,
+					"user",
+					CancellationToken.None,
+					new FileDataStore(credPath, true)).Result;
+			}
+
+			// Create Google Calendar API service.
+			var service = new CalendarService(new BaseClientService.Initializer() {
+				HttpClientInitializer = credential,
+				ApplicationName = ApplicationName,
+			});
+			
+			return service;
+		}
+	}
+}
---
+++ MarvelComicCalanderUpdater/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using RestSharp;
+using RestSharp.Deserializers;
+using System.Security.Cryptography;
+
+namespace MarvelComicCalanderUpdater {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+		private string publicKey = ConfigurationManager.AppSettings["publicKey"];
+		private string privateKey = ConfigurationManager.AppSettings["privateKey"];
+		private RestClient Client;
+		List<Comics.Result> comicList;
+
+        public MainWindow() {
+            InitializeComponent();
+			Client = new RestClient("http://gateway.marvel.com/v1/public/");
+        }
+
+		public RestResponse<T> APIRequest<T>(RestRequest request) where T : new(){
+			string timeStamp = Math.Floor((DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0)).TotalSeconds).ToString();
+			request.AddParameter("ts", timeStamp);
+			request.AddParameter("apikey", publicKey);
+			request.AddParameter("hash", getMD5(timeStamp));
+
+			RestResponse<T> response = (RestResponse<T>)Client.Execute<T>(request);
+			return response;
+		}
+
+
+		private List<Series.Result> searchSeries(string searchTerm) {
+			RestRequest seriesRequest = new RestRequest("/series", Method.GET);
+			seriesRequest.AddParameter("title", searchTerm);
+			RestResponse<Series.RootObject> response = APIRequest<Series.RootObject>(seriesRequest);
+			List<Series.Result> seriesList = response.Data.data.results;
+			return seriesList;
+		}
+
+		private List<Comics.Result> searchComics(string searchTerm) {
+			RestRequest comicsRequest = new RestRequest("/comics", Method.GET);
+			comicsRequest.AddParameter("title", searchTerm);
+			RestResponse<Comics.RootObject> response = APIRequest<Comics.RootObject>(comicsRequest);
+			List<Comics.Result> comicsList = response.Data.data.results;
+			return comicsList;
+		}
+
+		private string getMD5(string timeStamp) {
+			using (MD5 md5Hash = MD5.Create()) {
+				// Convert the input string to a byte array and compute the hash. 
+				byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(timeStamp + privateKey + publicKey));
+
+				// Create a new Stringbuilder to collect the bytes 
+				// and create a string.
+				StringBuilder sBuilder = new StringBuilder();
+
+				// Loop through each byte of the hashed data  
+				// and format each one as a hexadecimal string. 
+				for (int i = 0; i < data.Length; i++) {
+					sBuilder.Append(data[i].ToString("x2"));
+				}
+
+				// Return the hexadecimal string. 
+				return sBuilder.ToString();
+			}
+		}
+
+		private void SearchButton_Click(object sender, RoutedEventArgs e) {
+			comicList = searchComics(searchBox.Text);
+			listBox.ItemsSource = comicList;
+			listBox.DisplayMemberPath = "title";
+		}
+
+		private void AddButton_Click(object sender, RoutedEventArgs e) {
+			Comics.Result test = (Comics.Result)listBox.SelectedItem;
+			GoogleCalender calender = new GoogleCalender();
+			calender.CreateEvent(listBox.Text, test.dates[0].date);
+		}
+
+    }
+}
+
